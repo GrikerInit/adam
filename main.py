00685930302c4daaf5baa50ef2b10b56274842d7
Paul Ranshaw <p.s.ranshaw@gmail.com>
2021-06-30 14:17:27 +0100

    Updated event on_message within main.py

--- main.py
+++ main.py
@@ -9,29 +9,13 @@ token = os.getenv("DISCORD_BOT_TOKEN")
 async def on_ready():
  print('Bot is ready')
 
-hi = "Hi son"
-weather = "How is the weather"
-hi1 = "hi son"
-weather2 = "how is the weather"
-
 @client.event
 async def on_message(message):
-    if message.content == hi:
-        await message.channel.send('hello daddy :smile:')
-
-@client.event
-async def on_message(message):
-    if message.content == weather:
+    if message.content.lower() == "hi son":
+        await message.channel.send("hello daddy :smile:")
+    elif message.content.lower() == "how is the weather":
         await message.channel.send('Weather is Good')
-
-@client.event
-async def on_message(message):
-    if message.content == hi1:
-        await message.channel.send('hello daddy :smile:')
-
-@client.event
-async def on_message(message):
-    if message.content == weather2:
-        await message.channel.send('Weather is Good')
+    else:
+        pass
 
 client.run(token)
